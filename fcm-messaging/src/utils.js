6cf55e61ddfdf7e62785ab8caf6ba4cb917db2e1
Dmitry Gorshkov <50143321+Rullflex@users.noreply.github.com>
2024-10-27 23:05:49 +0500

    Update utils.js

--- fcm-messaging/src/utils.js
+++ fcm-messaging/src/utils.js
@@ -12,7 +12,7 @@ admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FCM_PROJECT_ID,
     clientEmail: process.env.FCM_CLIENT_EMAIL,
-    privateKey: process.env.FCM_PRIVATE_KEY,
+    privateKey: process.env.FCM_PRIVATE_KEY.replace(/\\n/gm, "\n"),
   }),
   databaseURL: process.env.FCM_DATABASE_URL,
 });
